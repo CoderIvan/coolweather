36e3c303750b954e9d45de1e6daf414c7c8dac72
Ivan <43811702@qq.com>
2014-10-02 01:05:48 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,68 @@
+package com.coolweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+
+	/**
+	 * Province表 建表语句
+	 */
+	public static final String CREATE_PROVINCE;
+	/**
+	 * City表 建表语句
+	 */
+	public static final String CREATE_CITY;
+	/**
+	 * Count表 建表语句
+	 */
+	public static final String CREATE_COUNTY;
+
+	static {
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("create table Province(");
+		builder.append("id integer primary key autoincrement,");
+		builder.append("province_name text,");
+		builder.append("province_code text");
+		builder.append(")");
+		CREATE_PROVINCE = builder.toString();
+		builder.setLength(0);
+
+		builder.append("create table City(");
+		builder.append("id integer primary key autoincrement,");
+		builder.append("city_name text,");
+		builder.append("city_code text");
+		builder.append(")");
+		CREATE_CITY = builder.toString();
+		builder.setLength(0);
+
+		builder.append("create table County(");
+		builder.append("id integer primary key autoincrement,");
+		builder.append("county_name text,");
+		builder.append("county_code text");
+		builder.append(")");
+		CREATE_COUNTY = builder.toString();
+		builder.setLength(0);
+
+	}
+
+	public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+
+}
